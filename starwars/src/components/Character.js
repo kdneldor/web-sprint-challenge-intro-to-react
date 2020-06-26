5b5129b8bac7285b3e428846ae014e17cf618a33
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-06-26 18:11:49 -0500

    Installed dependancies and starting beginning of character component build

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,9 @@
 // Write your Character component here
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+function Character(){
+    const [characters, setCharacters] = useState([])
+
+    useEffect
+}
